d76d7d3b47a171ddc52b0ffcce9067c4e7bf48b9
Pietro Napoleone <napoleone.2195695@studenti.uniroma1.it>
2025-10-30 16:02:10 +0100

    add transf

---
+++ source/model/transformer.py
@@ -0,0 +1,20 @@
+class TransformerBlock(nn.Module):
+  def __init__(self, k, heads):
+    super().__init__()
+
+    self.attention = SelfAttention(k, heads=heads)
+
+    self.norm1 = nn.LayerNorm(k)
+    self.norm2 = nn.LayerNorm(k)
+
+    self.ff = nn.Sequential(  # usually the hidden layer is bigger than the input
+      nn.Linear(k, 4 * k),
+      nn.ReLU(),
+      nn.Linear(4 * k, k))
+
+  def forward(self, x):
+    attended = self.attention(x)
+    x = self.norm1(attended + x)
+
+    fedforward = self.ff(x)
+    return self.norm2(fedforward + x)
